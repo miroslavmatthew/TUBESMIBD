bb91e184c565ca9ee17d35a9a8d3c288a2c107df
miroslavmatthew <90718789+miroslavmatthew@users.noreply.github.com>
2023-06-11 11:04:18 +0700

    db alamat

--- Database/data.sql
+++ Database/data.sql
@@ -76,13 +76,24 @@ ALTER TABLE `Transaksi` ADD CONSTRAINT `FK-TikTrans` FOREIGN KEY (`idTiket`) REF
 
 ALTER TABLE MejaB ADD CONSTRAINT UnikNo UNIQUE (posisiM);
 
-INSERT INTO `User` (`idU`, `UsernameAdmin`, `PasswordAdmin`, `Username`, `Password`, `namaMember`, `Alamat`, `namaPemesan`, `idKelurahan`) VALUES ('1', 'admin', 'XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=', NULL, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO `User` (`idU`, `UsernameAdmin`, `PasswordAdmin`, `Username`, `Password`, `namaMember`, `Alamat`, `namaPemesan`, `idKelurahan`) VALUES ('2', NULL, NULL, 'andi', 'XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=', 'andi', 'Jl. Andir No. 20', NULL, '1');
-
 INSERT INTO `Kota` (`idKota`, `namaKota`) VALUES ('1', 'Bandung'),('2', 'Cimahi'), ('3', 'Sukabumi'), ('4', 'Cirebon'), ('5', 'Bogor');
 INSERT INTO `Kecamatan` (`idKecamatan`, `namaKecamatan`, `idKota`) VALUES ('1', 'Andir', '1'), ('2', 'Antapani', '1');
 INSERT INTO `Kecamatan` (`idKecamatan`, `namaKecamatan`, `idKota`) VALUES ('3', 'Cimahi Utara', '2'), ('4', 'Cimahi Selatan', '2');
 INSERT INTO `Kecamatan` (`idKecamatan`, `namaKecamatan`, `idKota`) VALUES ('5', 'Ciracap', '3'), ('6', 'Cisolok', '3');
-
-
+INSERT INTO `Kecamatan` (`idKecamatan`, `namaKecamatan`, `idKota`) VALUES ('7', 'Jamblang', '4'), ('8', 'Gempol', '4');
+INSERT INTO `Kecamatan` (`idKecamatan`, `namaKecamatan`, `idKota`) VALUES ('9', 'Bogor Barat', '5'), ('10', 'Bogor Timur', '5');
 INSERT INTO `Kelurahan` (`idKelurahan`, `namaKelurahan`, `idKecamatan`) VALUES ('1', 'Ciroyom', '1'), ('2', 'Kebonjeruk', '1');
+INSERT INTO `Kelurahan` (`idKelurahan`, `namaKelurahan`, `idKecamatan`) VALUES ('3', 'Antapani Kidul', '2'), ('4', 'Antapani Kulon', '2');
+INSERT INTO `Kelurahan` (`idKelurahan`, `namaKelurahan`, `idKecamatan`) VALUES ('7', 'Cibabat', '3'), ('8', 'Citeureup', '3');
+INSERT INTO `Kelurahan` (`idKelurahan`, `namaKelurahan`, `idKecamatan`) VALUES ('5', 'Cibeber', '4'), ('6', 'Cibeureum', '4');
+INSERT INTO `Kelurahan` (`idKelurahan`, `namaKelurahan`, `idKecamatan`) VALUES ('9', 'Cikangkung', '5'), ('10', 'Pasirpanjang', '5');
+INSERT INTO `Kelurahan` (`idKelurahan`, `namaKelurahan`, `idKecamatan`) VALUES ('11', 'Gunungtanjung', '6'), ('12', 'Pasirbaru', '6');
+INSERT INTO `Kelurahan` (`idKelurahan`, `namaKelurahan`, `idKecamatan`) VALUES ('13', 'Bakung Kidul', '7'), ('14', 'Bakung Lor', '7');
+INSERT INTO `Kelurahan` (`idKelurahan`, `namaKelurahan`, `idKecamatan`) VALUES ('15', 'Cupang', '8'), ('16', 'Winong', '8');
+INSERT INTO `Kelurahan` (`idKelurahan`, `namaKelurahan`, `idKecamatan`) VALUES ('17', 'Menteng', '9'), ('18', 'Margajaya', '9');
+INSERT INTO `Kelurahan` (`idKelurahan`, `namaKelurahan`, `idKecamatan`) VALUES ('19', 'Sindangsari', '10'), ('20', 'Sindangrasa', '10');
+
+INSERT INTO `User` (`idU`, `UsernameAdmin`, `PasswordAdmin`, `Username`, `Password`, `namaMember`, `Alamat`, `namaPemesan`, `idKelurahan`) VALUES ('1', 'admin', 'XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=', NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `User` (`idU`, `UsernameAdmin`, `PasswordAdmin`, `Username`, `Password`, `namaMember`, `Alamat`, `namaPemesan`, `idKelurahan`) VALUES ('2', NULL, NULL, 'andi', 'XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=', 'andi', 'Jl. Andir No. 20', NULL, '1');
+
+
