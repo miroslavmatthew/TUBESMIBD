f96c1884498b447b95882dbcca0f01d208e509de
miroslavmatthew <90718789+miroslavmatthew@users.noreply.github.com>
2023-06-12 12:54:40 +0700

    ajax sign up

--- public/asset/signup.js
+++ public/asset/signup.js
@@ -1,11 +1,12 @@
 let select = document.querySelector("select[name='district']");
+let selectSub = document.querySelector("select[name='subDistrict']");
+let selectUrban = document.querySelector("select[name='urbanVillage']");
 let kots;
 const promise = fetch("kota")
   .then(function (response) {
     return response.text();
   })
   .then(function (text) {
-    console.log(text);
     kots = JSON.parse(text).kota;
     let select = document.querySelector("select[name='district']");
     for (let index = 0; index < kots.length; index++) {
@@ -18,18 +19,15 @@ const promise = fetch("kota")
   });
 
 select.addEventListener("input", function () {
-  let selectSub = document.querySelector("select[name='subDistrict']");
-  while (selectSub.children.length > 1) {
+  while (selectSub.firstChild) {
     selectSub.removeChild(selectSub.lastChild);
   }
-
-  selectSub.firstChild.selected = true;
+  selectSub.disabled = false;
   const promise = fetch(`kecamatan?idKota=${select.value}`)
     .then(function (response) {
       return response.text();
     })
     .then(function (text) {
-      console.log(text);
       kecs = JSON.parse(text).kecamatan;
       for (let index = 0; index < kecs.length; index++) {
         let option = document.createElement("option");
@@ -38,7 +36,47 @@ select.addEventListener("input", function () {
         option.value = namakecs.idKecamatan;
         selectSub.add(option);
       }
+      let defValue = document.createElement("option");
+      defValue.selected = true;
+      defValue.disabled = true;
+      defValue.hidden = true;
+      defValue.textContent = "Pilih Kecamatan";
+      selectSub.add(defValue);
 
-      selectSub.disabled = false;
+      let defValueUrb = document.createElement("option");
+      defValueUrb.selected = true;
+      defValueUrb.disabled = true;
+      defValueUrb.hidden = true;
+      defValueUrb.textContent = "Pilih Kelurahan";
+      selectUrban.add(defValueUrb);
+
+      selectUrban.disabled = true;
+    });
+});
+
+selectSub.addEventListener("input", function () {
+  while (selectUrban.firstChild) {
+    selectUrban.removeChild(selectUrban.lastChild);
+  }
+  selectUrban.disabled = false;
+  const promise = fetch(`kelurahan?idKecamatan=${selectSub.value}`)
+    .then(function (response) {
+      return response.text();
+    })
+    .then(function (text) {
+      kels = JSON.parse(text).kelurahan;
+      for (let index = 0; index < kels.length; index++) {
+        let option = document.createElement("option");
+        let namakels = kels[index];
+        option.text = `${namakels.namaKelurahan}`;
+        option.value = namakels.idKelurahan;
+        selectUrban.add(option);
+        let defValue = document.createElement("option");
+        defValue.selected = true;
+        defValue.disabled = true;
+        defValue.hidden = true;
+        defValue.textContent = "Pilih Kelurahan";
+        selectUrban.add(defValue);
+      }
     });
 });
