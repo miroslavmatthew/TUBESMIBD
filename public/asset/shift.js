458297807eac48f843029df1ef515743185904ad
kevchristian <135082813+kevchristian@users.noreply.github.com>
2023-06-15 08:45:11 +0700

    admin (tambahan pop up)

--- public/asset/shift.js
+++ public/asset/shift.js
@@ -5,3 +5,19 @@ function openForm() {
 function closeForm() {
     document.getElementById("popupForm").style.display = "none";
 }
+
+function openForm2() {
+    document.getElementById("popupForm2").style.display = "block";
+}
+
+function closeForm2() {
+    document.getElementById("popupForm2").style.display = "none";
+}
+
+function openForm3() {
+    document.getElementById("popupForm3").style.display = "block";
+}
+
+function closeForm3() {
+    document.getElementById("popupForm3").style.display = "none";
+}
