2eb0a1b359d7ba439ea97351272f309ff24a8b2d
guguboo <135086306+guguboo@users.noreply.github.com>
2023-06-17 20:39:12 +0700

    trans history

--- public/asset/trans_history.js
+++ public/asset/trans_history.js
@@ -1,4 +1,14 @@
 function openForm() {
+
+    let tiket = JSON.parse(document.querySelector(".statusBtnBooked").value)
+    document.getElementById("formTable").textContent = "Table#" + tiket.noMeja;
+    let tglFormatted = tiket.Tanggal.split("T")[0];
+    document.getElementById("formDate").textContent = tglFormatted;
+    document.getElementById("formStart").textContent = tiket.Jam;
+    let start = Number(tiket.Jam.split(":")[0]) + 1;
+    let end = start.toString().padStart(2, '0') + ":00:00";
+    document.getElementById("formEnd").textContent = end;
+    document.getElementById("formStat").textContent = tiket.Status + "\n\n\nPlease redeem this ticket to the receptionist!";
     document.getElementById("popupForm").style.display = "block";
 }
 
