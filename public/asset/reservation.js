c6b7385aadf2e0fc4c6f17b9005eefd09845ab19
guguboo <135086306+guguboo@users.noreply.github.com>
2023-06-18 21:57:37 +0700

    perbaikan date

--- public/asset/reservation.js
+++ public/asset/reservation.js
@@ -1,3 +1,12 @@
+var today = new Date();
+var year = today.getFullYear();
+var month = String(today.getMonth() + 1).padStart(2, '0');
+var day = String(today.getDate()).padStart(2, '0');
+var formattedDate = year + '-' + month + '-' + day;
+
+var hours = String(today.getHours()).padStart(2, '0');
+document.querySelector("#date").min = formattedDate;
+
 let select = document.querySelector("select[name='time']");
 function n(n) {
   return n > 9 ? "" + n : "0" + n;
@@ -9,16 +18,24 @@ for (let index = 8; index <= 21; index++) {
   option.value = `${n(index)}:00:00`;
   select.add(option);
 }
+
 function validate() {
   let dates = document.querySelector("#date").value;
   let times = document.querySelector("#time").value;
 
   let validated = false;
   if (dates == "") {
-    errMsg = "*Enter date please!*";
+    errMsg = "*Enter date!*";
   } else if (times == "") {
-    errMsg = "*Choose Jam Bermain please!*";
-  } else {
+    errMsg = "*Choose Jam Bermain!*";
+  } else if(dates == formattedDate){
+    if(times <= hours){
+      errMsg = "*Enter valid Jam Bermain!*";
+    }
+    else{
+      validated = true;
+    }
+  } else{
     validated = true;
   }
 
